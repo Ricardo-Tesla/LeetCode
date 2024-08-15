7d692c4d4e2a0742dc43cff69e260aa845d0df35
Ricardo-Tesla <148558452+Ricardo-Tesla@users.noreply.github.com>
2024-08-15 12:01:52 +0300

    Add files via upload

---
+++ Arrays/PlusOne.py
@@ -0,0 +1,14 @@
+def plusOne(nums):
+    n=len(nums)
+    
+    for i in range(n-1, -1, -1):
+        if nums[i] < 9:
+            nums[i] +=1
+            return nums
+        
+        nums[i]=0
+    
+    return [1]+ nums
+
+nums=[1,2,9]
+print(plusOne(nums))
